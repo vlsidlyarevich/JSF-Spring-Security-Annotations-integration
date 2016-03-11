768d3b1ed8d2b9e664d3c61e52e4d4868f490c56
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-03-11 16:19:54 +0300

    SecurityInitializer implemented

--- src/main/java/com/github/vlsidlyarevich/JSFSpringAnnotations/JSFSpringSecurity/security/configuration/SecurityInitializer.java
+++ src/main/java/com/github/vlsidlyarevich/JSFSpringAnnotations/JSFSpringSecurity/security/configuration/SecurityInitializer.java
@@ -1,5 +1,22 @@
 package com.github.vlsidlyarevich.JSFSpringAnnotations.JSFSpringSecurity.security.configuration;
 
 
-public class SecurityInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SecurityInitializer  extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses()  {
+        return new Class[] { AppConfiguration.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
 }
