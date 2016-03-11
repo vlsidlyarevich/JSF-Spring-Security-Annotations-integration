d64521a4acc3a555e0e53fa4490d4f3ba19a5a10
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-03-11 14:54:37 +0300

    SecurityConfiguration added

---
+++ src/main/java/com/github/vlsidlyarevich/JSFSpringAnnotations/JSFSpringSecurity/security/configuration/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.github.vlsidlyarevich.JSFSpringAnnotations.JSFSpringSecurity.security.configuration;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("vlad").password("123").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("karina").password("345").roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+                .and().formLogin()
+                .and().exceptionHandling().accessDeniedPage("/Access_Denied");
+    }
+
+}
